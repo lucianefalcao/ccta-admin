f47efb79e5603cd29a5213c0f4ce3638ca502110
Luciane Falcão <lucianefalcaoff@gmail.com>
2021-11-13 07:29:09 -0300

    setup project and create login screen

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,10 +51,10 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
+        light: {
+          primary: '#FF5252',
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
           info: colors.teal.lighten1,
--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,96 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+  <v-container class="auth-wrapper pa-0">
+    <v-row justify="space-around">
+      <v-card class="auth-card" width="400" height="400">
+        <v-card-title class="d-flex align-center justify-center">
+          <v-img
+            src="/logo.svg"
+            max-height="30px"
+            max-width="30px"
+            alt="logo"
+            class="me-3"
+            contain
+          />
+          <p class="text-2xl font-weight-medium mb-0">
+            CCTA
+          </p>
         </v-card-title>
+
         <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
+          <p class="text-2xl font-weight-medium text--primary mb-2">
+            Bem vindo(a) ao CCTA Admin!
           </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
+          <p class="mb-2">
+            Por favor, realize o login
           </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
         </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
+
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              v-model="email"
+              outlined
+              label="Email"
+              hide-details
+              class="mb-3"
+            />
+            <v-text-field
+              v-model="password"
+              :type="isPasswordVisible ? 'text' : 'password'"
+              :append-icon="isPasswordVisible ? icons.mdiEyeOffOutline : icons.mdiEyeOutline"
+              outlined
+              label="Senha"
+              hide-details
+              class="mb-3"
+              @click:append="isPasswordVisible = !isPasswordVisible"
+            />
+
+            <v-btn
+              block
+              color="primary"
+              class="mt-6"
+            >
+              Login
+            </v-btn>
+          </v-form>
+        </v-card-text>
       </v-card>
-    </v-col>
-  </v-row>
+    </v-row>
+  </v-container>
 </template>
+
+<script lang="ts">
+
+import { Vue, Component } from 'vue-property-decorator'
+import { mdiEyeOffOutline, mdiEyeOutline } from '@mdi/js'
+
+@Component({
+  layout: 'empty'
+})
+export default class Index extends Vue {
+  email: string = ''
+  password: string = ''
+  isPasswordVisible: Boolean = false
+  icons: Object = {
+    mdiEyeOffOutline,
+    mdiEyeOutline
+  }
+}
+</script>
+
+<style>
+.auth-card {
+  padding: 0.9375rem 0.875rem;
+}
+.text-2xl {
+  font-size: 1.5rem !important;
+  line-height: 2rem !important;
+}
+.auth-wrapper {
+  display: flex;
+  min-height: 95vh;
+  width: 100%;
+  flex-basis: 100%;
+  align-items: center;
+}
+</style>
