a7f4cf0c99a3745a316f4602fc30f2a5aa490196
Luciane Falcão <lucianefalcaoff@gmail.com>
2021-11-15 17:37:26 -0300

    create drawer menu

---
+++ components/NavMenuItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-list-item class="ps-5" active-class="primary text--white" link>
+    <v-list-item-icon class="mr-5">
+      <v-icon>
+        {{ icon }}
+      </v-icon>
+    </v-list-item-icon>
+
+    <v-list-item-content>
+      <v-list-item-title>{{ title }}</v-list-item-title>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script lang="ts">
+
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component
+export default class NavMenuItem extends Vue {
+  @Prop({ type: String, required: true, default: '' })
+  title!: String
+
+  @Prop({ type: String, required: true })
+  icon!: String
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,86 +1,41 @@
 <template>
-  <v-app
-    app
-    flat
-    absolute
-    color="transparent"
-  >
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
+  <v-app>
+    <vertical-nav-menu />
+
+    <v-app-bar app flat absolute color="transparent">
       <v-spacer />
+      <v-avatar size="40" color="#ff4c51">
+        <span class="white--text text-h5">{{ userName[0] }}</span>
+      </v-avatar>
     </v-app-bar>
-    <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
-    </v-main>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
-<script>
-export default {
-  data () {
-    return {
-      clipped: false,
-      drawer: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'CCTA'
-    }
+<script lang="ts">
+
+import { Component, Vue } from 'vue-property-decorator'
+import { userStore } from '@/store'
+import VerticalNavMenu from '@/components/VerticalNavMenu.vue'
+
+@Component({
+  components: {
+    VerticalNavMenu
+  }
+})
+export default class Default extends Vue {
+  get userName (): String {
+    return userStore.authUser?.name!
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.app-title {
+  font-size: 1.25rem;
+  font-weight: 700;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: 0.3px;
+}
+</style>
