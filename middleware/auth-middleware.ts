7022f2934db18d440412334c530eeac8f40923b6
Luciane Falcão <lucianefalcaoff@gmail.com>
2021-11-15 15:44:01 -0300

    correct nullable object and redirect after login

--- middleware/auth-middleware.ts
+++ middleware/auth-middleware.ts
@@ -2,7 +2,7 @@ import { Middleware } from '@nuxt/types'
 import { userStore } from '@/store'
 
 const authMiddleware: Middleware = ({ route, redirect }) => {
-  if (route.path !== '/' && !userStore.authUser.uid) {
+  if (route.path !== '/' && !userStore.authUser?.uid) {
     return redirect('/')
   }
 }
