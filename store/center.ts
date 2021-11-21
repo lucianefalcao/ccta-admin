eca9cd4989f1f1f6755399efa9a0070360459891
Luciane Falcão <36483575+lucianefalcao@users.noreply.github.com>
2021-11-21 18:33:59 -0300

    fix #32 tela principal do centro (#33)

---
+++ store/center.ts
@@ -0,0 +1,32 @@
+import { Action, Module, VuexModule } from 'vuex-module-decorators'
+import Center from '@/models/domain/Center'
+import CenterTransformer from '@/transformers/center-transformer'
+
+@Module({ name: 'center', stateFactory: true, namespaced: true })
+export default class CenterModule extends VuexModule {
+  @Action({ rawError: true })
+  async save (center: Center): Promise<Center> {
+    const centerFirebase = CenterTransformer.transformModelToInfra(center)
+    const centerRef = await this.store.$fire.firestore.collection('center').doc()
+    await centerRef.set(centerFirebase)
+    center.uid = centerRef.id
+    return center
+  }
+
+  @Action({ rawError: true })
+  async getCenterInfo (): Promise<Center> {
+    const centerFirestore = await this.store.$fire.firestore.collection('center').get()
+    const centerData: any = centerFirestore.docs[0]
+
+    return centerData ? await CenterTransformer.transformInfraToModel(centerData.data(), centerData.id) : { about: '', localization: '' }
+  }
+
+  @Action({ rawError: true })
+  async update (center: Center): Promise<Center> {
+    const centerFirebase = CenterTransformer.transformModelToInfra(center)
+    const centerRef = await this.store.$fire.firestore.collection('center').doc(center.uid)
+    await centerRef.update(centerFirebase)
+
+    return center
+  }
+}
--- utils/store-accessor.ts
+++ utils/store-accessor.ts
@@ -4,17 +4,23 @@ import userModule from '@/store/users'
 import newsModule from '@/store/news'
 import editaisModule from '@/store/editais'
 import eventsModule from '@/store/events'
+import centerModule from '@/store/center'
+import courseModule from '@/store/course'
 
 let userStore: userModule
 let newsStore: newsModule
 let editaisStore: editaisModule
 let eventsStore: eventsModule
+let centerStore: centerModule
+let courseStore: courseModule
 
 function initialiseStores (store: Store<any>): void {
   userStore = getModule(userModule, store)
   newsStore = getModule(newsModule, store)
   editaisStore = getModule(editaisModule, store)
   eventsStore = getModule(eventsModule, store)
+  centerStore = getModule(centerModule, store)
+  courseStore = getModule(courseModule, store)
 }
 
-export { initialiseStores, userStore, newsStore, editaisStore, eventsStore }
+export { initialiseStores, userStore, newsStore, editaisStore, eventsStore, centerStore, courseStore }
