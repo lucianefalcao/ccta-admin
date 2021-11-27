f4089d720441a6ea8b0d29d821c525ab506eaeb8
Luciane Falcão <lucianefalcaoff@gmail.com>
2021-11-27 05:38:03 -0300

    fix #47 create user index page

---
+++ transformers/user-transformer.ts
@@ -0,0 +1,19 @@
+import User from '@/models/domain/User'
+import UserFirebase from '@/models/firebase/UserFirebase'
+
+export default class UserTransformer {
+  static transformInfraToModel (user: UserFirebase, userUid: String): User {
+    return {
+      uid: userUid,
+      email: user.email,
+      name: user.name
+    }
+  }
+
+  static transformModelToInfra (user: User): UserFirebase {
+    return {
+      email: user.email!,
+      name: user.name!
+    }
+  }
+}
