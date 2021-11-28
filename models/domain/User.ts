a83d38164bf54b837ca3891be9d22a0d97f12cd8
Luciane Falcão <lucianefalcaoff@gmail.com>
2021-11-28 06:44:35 -0300

    change signature

--- models/domain/User.ts
+++ models/domain/User.ts
@@ -1,6 +1,8 @@
+import Permission from './Permission'
+
 export default interface User {
   uid?: String,
   email?: String,
   name?: String,
-  permissions: String[]
+  permissions: Permission[]
 }
--- transformers/user-transformer.ts
+++ transformers/user-transformer.ts
@@ -1,17 +1,15 @@
 import User from '@/models/domain/User'
 import UserFirebase from '@/models/firebase/UserFirebase'
 import { permissionStore } from '@/store'
-import Permission from '~/models/domain/Permission'
 
 export default class UserTransformer {
   static async transformInfraToModel (user: UserFirebase, userUid: String): Promise<User> {
     const userPermissions = await permissionStore.getPermissionsByUserUid(userUid)
-    const permissions = userPermissions.map((item: Permission) => item.code) as String[]
     return {
       uid: userUid,
       email: user.email,
       name: user.name,
-      permissions
+      permissions: userPermissions
     }
   }
 
